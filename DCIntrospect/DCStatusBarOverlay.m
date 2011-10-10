6a4ff1729aa655936aa731b10690151575865525
Jonathan Saggau <saggau@gmail.com>
2011-10-10 04:21:47 -0400

    https://raw.github.com/gradha/DCIntrospect/548afd9cbec17f210b902d4c592d1419da58a7c1/DCIntrospect/DCStatusBarOverlay.m

--- DCIntrospect/DCStatusBarOverlay.m
+++ DCIntrospect/DCStatusBarOverlay.m
@@ -25,15 +25,22 @@
 {
     if ((self = [super initWithFrame:CGRectZero]))
 	{
-        self.windowLevel = UIWindowLevelStatusBar + 1.0f;
-        self.frame = [[UIApplication sharedApplication] statusBarFrame];
+		self.windowLevel = UIWindowLevelStatusBar + 1.0f;
+		UIInterfaceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
+		CGFloat screenWidth = [UIScreen mainScreen].bounds.size.width;
+		CGFloat screenHeight = [UIScreen mainScreen].bounds.size.height;
+		const CGFloat bar_size = 20;
+		if (UIInterfaceOrientationIsLandscape(orientation))
+			self.frame = CGRectMake(0, 0, screenHeight, bar_size);
+		else
+			self.frame = CGRectMake(0, 0, screenWidth, bar_size);
 		self.backgroundColor = [UIColor blackColor];
-
+        
         UIImageView *backgroundImageView = [[UIImageView alloc] initWithFrame:self.frame];
         backgroundImageView.image = [[UIImage imageNamed:@"statusBarBackground.png"] stretchableImageWithLeftCapWidth:2.0f topCapHeight:0.0f];
         [self addSubview:backgroundImageView];
         [backgroundImageView release];
-
+        
 		self.leftLabel = [[[UILabel alloc] initWithFrame:CGRectOffset(self.frame, 2.0f, 0.0f)] autorelease];
 		self.leftLabel.backgroundColor = [UIColor clearColor];
 		self.leftLabel.textAlignment = UITextAlignmentLeft;
@@ -41,7 +48,7 @@
 		self.leftLabel.textColor = [UIColor colorWithWhite:0.97f alpha:1.0f];
 		self.leftLabel.autoresizingMask = UIViewAutoresizingFlexibleWidth;
 		[self addSubview:self.leftLabel];
-
+        
 		self.rightLabel = [[[UILabel alloc] initWithFrame:CGRectOffset(self.frame, -2.0f, 0.0f)] autorelease];
 		self.rightLabel.backgroundColor = [UIColor clearColor];
 		self.rightLabel.font = [UIFont boldSystemFontOfSize:12.0f];
@@ -49,14 +56,14 @@
 		self.rightLabel.textColor = [UIColor colorWithWhite:0.9f alpha:1.0f];
 		self.rightLabel.autoresizingMask = UIViewAutoresizingFlexibleWidth;
 		[self addSubview:self.rightLabel];
-
+        
 		UITapGestureRecognizer *gestureRecognizer = [[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapped)] autorelease];
 		[self addGestureRecognizer:gestureRecognizer];
-
+        
 		[[UIDevice currentDevice] beginGeneratingDeviceOrientationNotifications];
 		[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(updateBarFrame) name:UIDeviceOrientationDidChangeNotification object:nil];
 	}
-
+    
 	return self;
 }
 
